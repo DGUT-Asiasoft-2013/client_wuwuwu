3659ed9266c8ced3fcc73b7b14b1cd6c4d7aef54
AceAya <452527641@qq.com>
2016-12-20 19:51:06 +0800

    ...

--- src/com/example/palmcampusmarket_client/MainActivity.java
+++ src/com/example/palmcampusmarket_client/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.palmcampusmarket_client;
 
+
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,5 +14,13 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		startPurchaseActivity();
+	
+	}
+	
+	void startPurchaseActivity(){
+		Intent itnt = new Intent(this, PurchaseActivity.class);
+		startActivity(itnt);
+		finish();
 	}
 }
