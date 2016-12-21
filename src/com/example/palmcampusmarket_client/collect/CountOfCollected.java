edec8f065a8d667f0c0e1cc8a3482cc3ac908d92
zjl0211 <270345068@qq.com>
2016-12-21 16:26:25 +0800

    显示已收藏人数

---
+++ src/com/example/palmcampusmarket_client/collect/CountOfCollected.java
@@ -0,0 +1,63 @@
+package com.example.palmcampusmarket_client.collect;
+
+import java.io.IOException;
+
+import com.example.palmcampusmarket_client.api.Server;
+import com.example.palmcampusmarket_client.api.entity.Commodity;
+
+import android.os.Handler;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class CountOfCollected {
+
+	public static interface OnCountResultListener{
+		void onResult(String result);
+	};
+
+	static Handler handler;
+	
+	public static void getCount(Commodity commodity, final OnCountResultListener listener){
+		if(handler==null) handler = new Handler();
+
+		Request requestLike = Server.requestBuilderWithApi("commodity/"+commodity.getId()+"/collected")
+				.build();
+
+		Server.getSharedClient().newCall(requestLike).enqueue(new Callback() {
+
+			@Override
+			public void onResponse(Call arg0, final Response arg1) throws IOException {
+				try{
+					final String num = arg1.body().string();
+					
+					handler.post(new Runnable() {
+						@Override
+						public void run() {
+							listener.onResult(num);		
+						}
+					});
+					
+				}catch(Exception ex){
+					handler.post(new Runnable() {
+						@Override
+						public void run() {
+							listener.onResult("???");		
+						}
+					});
+				}
+			}
+
+			@Override
+			public void onFailure(Call arg0, IOException arg1) {
+				handler.post(new Runnable() {
+					@Override
+					public void run() {
+						listener.onResult("???");		
+					}
+				});
+			}
+		});		
+	}
+}
