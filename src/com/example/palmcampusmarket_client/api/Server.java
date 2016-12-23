ee16e3e96425abf12a4c79130c9457bb393ad9c1
YTFree <475787983@qq.com>
2016-12-23 20:15:49 +0800

    界面优化

--- src/com/example/palmcampusmarket_client/api/Server.java
+++ src/com/example/palmcampusmarket_client/api/Server.java
@@ -25,7 +25,7 @@ public class Server {
 	}
 
 
-	public static String serverAddress = "http://192.168.253.6:8080/membercenter/";
+	public static String serverAddress = "http://172.27.15.10:8080/membercenter/";
 
 	public  static Request.Builder requestBuilderWithWallet(String wallet){
 		return new Request.Builder()
