b35a2df554dbd387722f2850a46ca53e278957e9
zjl0211 <270345068@qq.com>
2016-12-26 20:05:27 +0800

    物品详情添加标签：物品、评价、卖家

--- src/com/example/palmcampusmarket_client/collect/CommodityContentActivity.java
+++ src/com/example/palmcampusmarket_client/collect/CommodityContentActivity.java
@@ -7,13 +7,19 @@ import com.example.palmcampusmarket_client.api.Server;
 import com.example.palmcampusmarket_client.api.entity.Commodity;
 import com.example.palmcampusmarket_client.collect.CountOfCollected.OnCountResultListener;
 import com.example.palmcampusmarket_client.fragment.ImageView;
+import com.example.palmcampusmarket_client.fragment.pages.HomeListFragment;
 
+import android.R.integer;
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.TextView;
 import android.widget.Toast;
 import okhttp3.Call;
@@ -23,9 +29,240 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class CommodityContentActivity extends Activity {
+	//	TextView btnBack;
+	//	TextView btnCollect;
+	//	Button btnBuy;		
+	//
+	//	@Override
+	//	protected void onCreate(Bundle savedInstanceState) {
+	//		// TODO Auto-generated method stub
+	//		super.onCreate(savedInstanceState);
+	//
+	//		setContentView(R.layout.activity_commodity_content);
+	//
+	//		final Commodity commodity = (Commodity) getIntent().getSerializableExtra("commodity");
+	//
+	//
+	//
+	//		btnBack = (TextView) findViewById(R.id.btn_back_commodity_content);
+	//
+	//		btnBack.setOnClickListener(new OnClickListener() {
+	//
+	//			@Override
+	//			public void onClick(View v) {
+	//				finish();
+	//
+	//			}
+	//		});
+	//
+	//		btnCollect = (TextView) findViewById(R.id.btn_collect_commodity_content);
+	//
+	//		checkIsCollected(commodity, new OnCheckResultListener() {
+	//
+	//			@Override
+	//			public void onResult(String result) {
+	//				if(result.equals("false")){
+	//					btnCollect.setText("☆");
+	//
+	//				}else{
+	//					btnCollect.setText("★");
+	//				}		
+	//
+	//			}
+	//		});
+	//
+	//		btnBuy = (Button) findViewById(R.id.btn_buy);
+	//
+	//		ImageView image;
+	//		TextView name;
+	//
+	//		final TextView countCollected;
+	//
+	//		TextView seller;
+	//		TextView num;
+	//		TextView price;
+	//		TextView describe;
+	//
+	//
+	//		image = (ImageView) findViewById(R.id.image_commodity_content);
+	//		image.load(Server.serverAddress + commodity.getCommImage());
+	//
+	//		name = (TextView) findViewById(R.id.name_commodity_content);
+	//		name.setText(commodity.getCommName());
+	//
+	//		countCollected = (TextView) findViewById(R.id.collected_commodity_content);
+	//
+	//		CountOfCollected.getCount(commodity, new OnCountResultListener() {
+	//
+	//			@Override
+	//			public void onResult(String result) {
+	//				// TODO Auto-generated method stub
+	//				countCollected.setText(result + "收藏");
+	//
+	//			}
+	//		});
+	//
+	//
+	//
+	//		seller = (TextView) findViewById(R.id.seller_commodity_content);
+	//		seller.setText(commodity.getUser().getNickname());
+	//		seller.setOnClickListener(new OnClickListener() {
+	//			
+	//			@Override
+	//			public void onClick(View v) {
+	//				// TODO Auto-generated method stub
+	//			}
+	//		});
+	//
+	//		num = (TextView) findViewById(R.id.num_commodity_content);
+	//		String s = String.valueOf(commodity.getCommNumber());
+	//		num.setText(s);
+	//
+	//		price = (TextView) findViewById(R.id.price_commodity_content);
+	//		price.setText(commodity.getCommPrice());
+	//
+	//		describe = (TextView) findViewById(R.id.describe_commodity_content);
+	//		describe.setText(commodity.getCommDescribe());
+	//
+	//
+	//
+	//		btnCollect.setOnClickListener(new OnClickListener() {
+	//
+	//			@Override
+	//			public void onClick(View v) {
+	//				checkIsCollected(commodity,new OnCheckResultListener() {
+	//
+	//					@Override
+	//					public void onResult(String result) {
+	//						// TODO Auto-generated method stub
+	//
+	//						if(result.equals("false")){
+	//							btnCollect.setText("☆");
+	//
+	//						}else{
+	//							btnCollect.setText("★");
+	//						}		
+	//						changeCollected(result,commodity);
+	//
+	//					}
+	//				});
+	//
+	//			}
+	//		});		
+	//	}
+	//
+	//	protected void callSeller(String telephone) {
+	//		// TODO Auto-generated method stub
+	//		
+	//	}
+	//
+	//	@Override
+	//	protected void onResume() {
+	//		// TODO Auto-generated method stub
+	//		super.onResume();
+	//	}
+	//
+	//	void changeCollected(String checkResult,final Commodity commodity){
+	//
+	//		String param = String.valueOf(checkResult.equals("false"));
+	//		MultipartBody body = new MultipartBody
+	//				.Builder()
+	//				.addFormDataPart("collect",param)
+	//				.build();
+	//
+	//		Request request = Server.requestBuilderWithCs("commodity/" + commodity.getId() + "/collect")
+	//				.post(body)
+	//				.build();
+	//
+	//		Server.getSharedClient().newCall(request).enqueue(new Callback() {
+	//
+	//			@Override
+	//			public void onResponse(Call arg0, Response arg1) throws IOException {
+	//				runOnUiThread(new Runnable() {					
+	//					@Override
+	//					public void run() {
+	//						checkIsCollected(commodity, new OnCheckResultListener() {
+	//
+	//							@Override
+	//							public void onResult(String result) {
+	//								if(result.equals("false")){
+	//									btnCollect.setText("☆");
+	//
+	//								}else{
+	//									btnCollect.setText("★");
+	//								}		
+	//
+	//							}
+	//						});
+	//
+	//					}
+	//				});
+	//
+	//			}
+	//
+	//			@Override
+	//			public void onFailure(Call arg0, IOException arg1) {
+	//				// TODO Auto-generated method stub
+	//
+	//			}
+	//		});
+	//
+	//
+	//	}
+	//
+	//
+	//	interface OnCheckResultListener{
+	//		void onResult(String result);
+	//	}
+	//
+	//	Handler handler;
+	//
+	//	void checkIsCollected(Commodity commodity,final OnCheckResultListener listener){
+	//
+	//		if(handler == null){
+	//			handler = new Handler();
+	//		}
+	//
+	//		Request request = Server.requestBuilderWithCs("commodity/" + commodity.getId().toString() + "/iscollected")
+	//				.build();
+	//
+	//		Server.getSharedClient().newCall(request).enqueue(new Callback() {
+	//
+	//			@Override
+	//			public void onResponse(Call arg0, final Response arg1) throws IOException {
+	//
+	//				try {
+	//					final String checkResult = arg1.body().string();
+	//
+	//					handler.post(new Runnable() {
+	//
+	//						@Override
+	//						public void run() {
+	//							// TODO Auto-generated method stub								
+	//
+	//							listener.onResult(checkResult);
+	//
+	//						}
+	//					});
+	//
+	//				} catch (IOException e) {
+	//					// TODO Auto-generated catch block
+	//					e.printStackTrace();
+	//				}
+	//
+	//			}
+	//
+	//			@Override
+	//			public void onFailure(Call arg0, IOException arg1) {
+	//				// TODO Auto-generated method stub
+	//
+	//			}
+	//		});		
+	//	}
+
 	TextView btnBack;
 	TextView btnCollect;
-	Button btnBuy;		
+	RadioGroup tabbar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +272,6 @@ public class CommodityContentActivity extends Activity {
 		setContentView(R.layout.activity_commodity_content);
 
 		final Commodity commodity = (Commodity) getIntent().getSerializableExtra("commodity");
-
-
 
 		btnBack = (TextView) findViewById(R.id.btn_back_commodity_content);
 
@@ -50,6 +285,30 @@ public class CommodityContentActivity extends Activity {
 		});
 
 		btnCollect = (TextView) findViewById(R.id.btn_collect_commodity_content);
+		
+		btnCollect.setOnClickListener(new OnClickListener() {
+			
+						@Override
+						public void onClick(View v) {
+							checkIsCollected(commodity,new OnCheckResultListener() {
+			
+								@Override
+								public void onResult(String result) {
+									// TODO Auto-generated method stub
+			
+									if(result.equals("false")){
+										btnCollect.setText("☆");
+			
+									}else{
+										btnCollect.setText("★");
+									}		
+									changeCollected(result,commodity);
+			
+								}
+							});
+			
+						}
+					});		
 
 		checkIsCollected(commodity, new OnCheckResultListener() {
 
@@ -65,85 +324,63 @@ public class CommodityContentActivity extends Activity {
 			}
 		});
 
-		btnBuy = (Button) findViewById(R.id.btn_buy);
-
 		ImageView image;
-		TextView name;
-
-		final TextView countCollected;
-
-		TextView seller;
-		TextView num;
-		TextView price;
-		TextView describe;
-
-
 		image = (ImageView) findViewById(R.id.image_commodity_content);
 		image.load(Server.serverAddress + commodity.getCommImage());
 
-		name = (TextView) findViewById(R.id.name_commodity_content);
-		name.setText(commodity.getCommName());
 
-		countCollected = (TextView) findViewById(R.id.collected_commodity_content);
 
-		CountOfCollected.getCount(commodity, new OnCountResultListener() {
+		getFragmentManager()
+		.beginTransaction()
+		.replace(R.id.content_checked,new CommodityContentFragment())
+		.commit();
+
+		tabbar = (RadioGroup) findViewById(R.id.rd_content);
+		tabbar.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
 			@Override
-			public void onResult(String result) {
+			public void onCheckedChanged(RadioGroup group, int checkedId) {
 				// TODO Auto-generated method stub
-				countCollected.setText(result + "收藏");
+				int tab = group.getCheckedRadioButtonId();
+
+				RadioButton rb = (RadioButton) findViewById(tab);
+
+				int index = 0;
+
+				if(rb.getText().equals("商品")){
+
+					index = 0;
+					changeContentFragment(index);
+
+
+				}else if(rb.getText().equals("评价")){
+
+					index = 1;
+					changeContentFragment(index);
+
+				}else if(rb.getText().equals("卖家")){
+
+
+					index = 2;
+					changeContentFragment(index);
+
+				}
+
 
 			}
 		});
 
 
-
-		seller = (TextView) findViewById(R.id.seller_commodity_content);
-		seller.setText(commodity.getUser().getNickname());
-
-		num = (TextView) findViewById(R.id.num_commodity_content);
-		String s = String.valueOf(commodity.getCommNumber());
-		num.setText(s);
-
-		price = (TextView) findViewById(R.id.price_commodity_content);
-		price.setText(commodity.getCommPrice());
-
-		describe = (TextView) findViewById(R.id.describe_commodity_content);
-		describe.setText(commodity.getCommDescribe());
-
-
-
-		btnCollect.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				checkIsCollected(commodity,new OnCheckResultListener() {
-
-					@Override
-					public void onResult(String result) {
-						// TODO Auto-generated method stub
-
-						if(result.equals("false")){
-							btnCollect.setText("☆");
-
-						}else{
-							btnCollect.setText("★");
-						}		
-						changeCollected(result,commodity);
-
-					}
-				});
-
-			}
-		});		
 	}
+
+
 
 	@Override
 	protected void onResume() {
 		// TODO Auto-generated method stub
-		super.onResume();
-	}
+		super.onResume();		
 
+	}
 	void changeCollected(String checkResult,final Commodity commodity){
 
 		String param = String.valueOf(checkResult.equals("false"));
@@ -240,6 +477,37 @@ public class CommodityContentActivity extends Activity {
 
 			}
 		});		
+	}
+
+	void changeContentFragment(int index){
+		Fragment newFrag = null;
+		switch(index){
+		case 0:{ 
+			newFrag = new CommodityContentFragment();
+			break;
+		}
+		case 1:{
+			newFrag = new CommodityContentFragment();
+			break;
+		}
+		case 2:{
+			newFrag = new CommodityContentFragment();
+			break;
+		}
+		default:{
+			newFrag = new CommodityContentFragment();
+			break;		
+		}
+		}
+
+		if(newFrag==null)
+			return;
+
+		getFragmentManager()
+		.beginTransaction()
+		.replace(R.id.content_checked, newFrag)
+		.commit();
+
 	}
 
 }
---
+++ src/com/example/palmcampusmarket_client/collect/CommodityContentFragment.java
@@ -0,0 +1,77 @@
+package com.example.palmcampusmarket_client.collect;
+
+import com.example.palmcampusmarket_client.R;
+import com.example.palmcampusmarket_client.api.entity.Commodity;
+import com.example.palmcampusmarket_client.collect.CountOfCollected.OnCountResultListener;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class CommodityContentFragment extends Fragment {
+
+	View view;
+	
+//	Commodity commodity;
+//	
+//	
+//	public Commodity commodity {
+//		return commodity;
+//	}
+//
+//
+//	public void setCommodity(Commodity commodity) {
+//		this.commodity = commodity;
+//	}
+
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		if(view == null){
+			view = inflater.inflate(R.layout.fragment_commodity_content, null);
+			final Commodity commodity = (Commodity) getActivity().getIntent().getSerializableExtra("commodity");
+			TextView name;
+			name = (TextView) view.findViewById(R.id.name_commodity_content);
+			name.setText(commodity.getCommName());
+			
+			final TextView countCollected;
+			countCollected = (TextView) view.findViewById(R.id.collected_commodity_content);
+			
+			CountOfCollected.getCount(commodity, new OnCountResultListener() {
+				
+				@Override
+				public void onResult(String result) {
+					// TODO Auto-generated method stub
+					countCollected.setText(result + "收藏");
+				}
+			});
+			
+			TextView sellerName;
+			sellerName = (TextView) view.findViewById(R.id.seller_commodity_content);
+			sellerName.setText(commodity.getUser().getNickname());
+			
+			TextView price;
+			price = (TextView) view.findViewById(R.id.price_commodity_content);
+			price.setText(commodity.getCommPrice());
+			
+			TextView num;
+			num = (TextView) view.findViewById(R.id.num_commodity_content);
+			num.setText(String.valueOf(commodity.getCommNumber()));
+			
+			TextView describe;
+			describe = (TextView) view.findViewById(R.id.describe_commodity_content);
+			describe.setText(commodity.getCommDescribe());
+			
+			Button btnBuy;
+			btnBuy = (Button) view.findViewById(R.id.btn_buy);
+			
+		}		
+		
+		return view;
+	}
+}
