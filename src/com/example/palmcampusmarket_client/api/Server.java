f967e84bc764393d4f7136edc684cf0898afe49c
LiLingke <584277415@qq.com>
2016-12-28 18:30:26 +0800

    新增图片下载类

--- src/com/example/palmcampusmarket_client/api/Server.java
+++ src/com/example/palmcampusmarket_client/api/Server.java
@@ -26,7 +26,7 @@ public class Server {
 
 
 
-	public static String serverAddress = "http://172.27.15.10:8080/membercenter/";
+	public static String serverAddress = "http://192.168.253.4:8080/membercenter/";
 
 
 	public  static Request.Builder requestBuilderWithWallet(String wallet){
