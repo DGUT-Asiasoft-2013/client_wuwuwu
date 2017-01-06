6f0f28f97779136e8a95a332fcc554c4cc2d3fee
jackcao1995 <hulinmin213@sina.com>
2017-01-06 18:49:52 +0800

    添加自动登录记住密码功能

--- src/com/example/palmcampusmarket_client/LoginActivity.java
+++ src/com/example/palmcampusmarket_client/LoginActivity.java
@@ -3,11 +3,17 @@ package com.example.palmcampusmarket_client;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.MultipartBody;
@@ -32,7 +38,9 @@ import android.os.Bundle;
 import android.view.View;
 
 public class LoginActivity extends Activity {
-	SimpleTextInputCellFragment2 fragPassword,fragAccount;
+	SimpleTextInputCellFragment2 fragPassword,fragAccount;  
+    private CheckBox rem_pw, auto_login;      
+    private SharedPreferences sp;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -66,7 +74,55 @@ public class LoginActivity extends Activity {
 		});
 
 		fragAccount=(SimpleTextInputCellFragment2)getFragmentManager().findFragmentById(R.id.input_account);
-		fragPassword=(SimpleTextInputCellFragment2)getFragmentManager().findFragmentById(R.id.input_password); 
+		fragPassword=(SimpleTextInputCellFragment2)getFragmentManager().findFragmentById(R.id.input_password);
+		sp = this.getSharedPreferences("userInfo", Context.MODE_WORLD_READABLE);
+		rem_pw = (CheckBox) findViewById(R.id.remember_password);  
+        auto_login = (CheckBox) findViewById(R.id.autologin);
+        if(sp.getBoolean("ISCHECK", false))  
+        {  
+          //设置默认是记录密码状态  
+          rem_pw.setChecked(true);  
+          fragAccount.setEditText(sp.getString("USER_NAME", ""));  
+          fragPassword.setEditText(sp.getString("PASSWORD", ""));  
+          //判断自动登陆多选框状态  
+          if(sp.getBoolean("AUTO_ISCHECK", false))  
+          {  
+                 //设置默认是自动登录状态  
+                 auto_login.setChecked(true);  
+                //跳转界面  
+                Intent intent = new Intent(LoginActivity.this,HomePageActivity.class);  
+                LoginActivity.this.startActivity(intent);  
+                  
+          }  
+        }
+        rem_pw.setOnCheckedChangeListener(new OnCheckedChangeListener() {  
+            public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {  
+                if (rem_pw.isChecked()) {  
+                      
+                    System.out.println("记住密码已选中");  
+                    sp.edit().putBoolean("ISCHECK", true).commit();  
+                      
+                }else {  
+                      
+                    System.out.println("记住密码没有选中");  
+                    sp.edit().putBoolean("ISCHECK", false).commit();  
+                      
+                }  
+  
+            }  
+        });
+        auto_login.setOnCheckedChangeListener(new OnCheckedChangeListener() {  
+            public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {  
+                if (auto_login.isChecked()) {  
+                    System.out.println("自动登录已选中");  
+                    sp.edit().putBoolean("AUTO_ISCHECK", true).commit();  
+  
+                } else {  
+                    System.out.println("自动登录没有选中");  
+                    sp.edit().putBoolean("AUTO_ISCHECK", false).commit();  
+                }  
+            }  
+        });
 	}
 	@Override
 	protected void onResume() {
@@ -96,11 +152,11 @@ public class LoginActivity extends Activity {
 
 
 
-		String account = fragAccount.getText();       //获取用户输入的账号
-		String password = fragPassword.getText();
+	    final String account = fragAccount.getText();       //获取用户输入的账号
+		final String password = fragPassword.getText();
 
 
-		password = MD5.getMD5(password);                   //密码变成哈希数
+		String password5 = MD5.getMD5(fragPassword.getText());                   //密码变成哈希数
 
 		OkHttpClient client=Server.getSharedClient();
 
@@ -135,6 +191,16 @@ public class LoginActivity extends Activity {
 						@Override
 						public void run() {
 							progressDialog.dismiss();
+							//登录成功和记住密码框为选中状态才保存用户信息  
+		                    if(rem_pw.isChecked())  
+		                    {  
+		                     //记住用户名、密码、  
+		                      Editor editor = sp.edit();  
+		                      editor.putString("USER_NAME",account);  
+		                      editor.putString("PASSWORD",password);  
+		                      editor.commit();  
+		                    }  
+		                    //跳转界面  
 							startHomePageActivity();
 						}
 					});	
