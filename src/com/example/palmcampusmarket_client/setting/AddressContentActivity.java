7ed6d8c9762788d7965bed31a01a8696b572f13f
freedom9630 <512110187@qq.com>
2017-01-07 11:11:13 +0800

    地址（完成）

--- src/com/example/palmcampusmarket_client/setting/AddressContentActivity.java
+++ src/com/example/palmcampusmarket_client/setting/AddressContentActivity.java
@@ -1,6 +1,7 @@
 package com.example.palmcampusmarket_client.setting;
 
 import com.example.palmcampusmarket_client.R;
+import com.example.palmcampusmarket_client.api.entity.Address;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -13,6 +14,7 @@ public class AddressContentActivity extends Activity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_addresscontent);
+		final Address address = (Address) getIntent().getSerializableExtra("address");
 		addressListFragment = (AddressListFragment) getFragmentManager().findFragmentById(R.id.address_content);
 	}
 
